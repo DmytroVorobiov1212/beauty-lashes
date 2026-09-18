be0e4f2ca4412def017f53c5b938740c4f3fb479
Dmytro Vorobiov <116897771+DmytroVorobiov1212@users.noreply.github.com>
2026-09-18 20:28:47 +0200

    Refactor home page to server component

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,15 @@
-'use client';
-
-import Contact from "@/components/Contact/Contact";
-import Footer from "@/components/Footer/Footer";
-import Gallery from "@/components/Gallery/Gallery";
-import Hero from "@/components/Hero/Hero";
-import Offerings from "@/components/Offerings/Offerings";
-// import Prices from "@/components/Prices/Prices";
-// import Services from "@/components/Services/Services";
-import Team from "@/components/Team/Team";
+import Contact from '@/components/Contact/Contact';
+import Footer from '@/components/Footer/Footer';
+import Gallery from '@/components/Gallery/Gallery';
+import Hero from '@/components/Hero/Hero';
+import Offerings from '@/components/Offerings/Offerings';
+import Team from '@/components/Team/Team';
 
 export default function Page() {
   return (
     <>
       <main>
         <Hero />
-        {/* <Services /> */}
-        {/* <Prices /> */}
         <Offerings />
         <Gallery />
         <Team />
